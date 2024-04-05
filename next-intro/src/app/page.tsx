364028d04caa1f746a2e44be7f42249c8cca0c46
jjfantini <jjman2811@gmail.com>
2024-04-05 08:39:20 -0400

    feat(css): added css styling module to use to style components

--- next-intro/src/app/page.tsx
+++ next-intro/src/app/page.tsx
@@ -1,20 +1,35 @@
 import Link from "next/link";
+import postgres from "postgres";
+import { Suspense } from "react";
+import styles from "./home.module.css";
+
+const sql = postgres(process.env.DATABASE_URL!);
+
+type Quiz = {
+  id: number;
+  title: string;
+};
+
+async function Quizzes() {
+  const quizzes: Quiz[] = await sql`SELECT * FROM quizzes`;
+  return (
+    <ul>
+      {quizzes.map((quiz) => (
+        <li key={quiz.id}>
+          <Link href={`/quiz/${quiz.id}`}>{quiz.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default function Home() {
   return (
     <section>
-      <h1>All Quizzes</h1>
-      <ul>
-        <li>
-          <Link href="/quiz/1">Quiz 1</Link>
-        </li>
-        <li>
-          <Link href="/quiz/2">Quiz 2</Link>
-        </li>
-        <li>
-          <Link href="/quiz/3">Quiz 3</Link>
-        </li>
-      </ul>
+      <h1 className={styles.title}>All Quizzes</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Quizzes/>
+      </Suspense>
     </section>
   );
 }
